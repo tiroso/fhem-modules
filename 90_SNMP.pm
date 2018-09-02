90360eb6297dae6384fe4f8f1d3c2b3b75910cf8
tiroso <sobisch.t@gmail.com>
2018-09-03 00:14:07 +0200

    Update 90_SNMP.pm

--- 90_SNMP.pm
+++ 90_SNMP.pm
@@ -8,7 +8,7 @@ use Socket;
 use Data::Dumper;
 
 my %SNMP_sets = (
-    "send"      => "textField",
+    "WriteSingleOID"      => "textField",
 );
 my %SNMP_gets = (
 	"ReadSingleOID"		=> "textField",
@@ -36,6 +36,7 @@ sub SNMP_Initialize($) {
     $hash->{AttrList} =
     "ReadSingleOID:textField "
     ."ReadListOID:textField "
+    ."WritePreDefined:textField "
     ."ReadingsValueCorrection:textField "
     ."ReadingsCorrection:textField "
     ."ReadingsToState:textField "
@@ -51,8 +52,8 @@ sub SNMP_Initialize($) {
     ."SNMPPrivProto:DES "
     ."SNMPPrivPass:textField "
     ."SNMPRemotePort:textField "
-    ."SNMPUseSprintValue:0,1"
-    ."SNMPUseLongNames:0,1"
+    ."SNMPUseSprintValue:0,1 "
+    ."SNMPUseLongNames:0,1 "
     ."vendor:textField "
     . $readingFnAttributes;
         
@@ -93,8 +94,8 @@ sub SNMP_AutoUpdate($){
 	my $name = $hash->{NAME};
       
     
-    if(scalar devspec2array("TYPE=SNMP:FILTER=RUNNING=1") > 0){
-        InternalTimer(gettimeofday()+int(5 + rand(30)), "SNMP_AutoUpdate", $hash);
+    if(scalar devspec2array("TYPE=SNMP:FILTER=RUNNING=1") >= AttrVal("global","SNMPMaxAutoUpdate",1)){
+        InternalTimer(gettimeofday()+int(1 + rand(5)), "SNMP_AutoUpdate", $hash);
     }else{
         my $jsoncoder = JSON::XS->new();
         $jsoncoder->allow_nonref();
@@ -107,17 +108,17 @@ sub SNMP_AutoUpdate($){
         
         unless(exists($hash->{Helper}{"GetOid"})){
             $hash->{"RUNNING"}=1;
-            $hash->{Helper}{"GetOid"} = BlockingCall("SNMP_GetOid", $name."|".$jsoncoder->encode(\%blocking_data),"SNMP_GetOidFinish", 30, "SNMP_GetOidAborted", $name."|".$jsoncoder->encode(\%blocking_data));            
+            $hash->{Helper}{"GetOid"} = BlockingCall("SNMP_GetOid", $name."|".$jsoncoder->encode(\%blocking_data),"SNMP_GetOidFinish", 15, "SNMP_GetOidAborted", $name."|".$jsoncoder->encode(\%blocking_data));            
         }
-        InternalTimer(gettimeofday()+int(45 + rand(45)), "SNMP_AutoUpdate", $hash);
+        InternalTimer(gettimeofday()+int(45 + rand(30)), "SNMP_AutoUpdate", $hash);
     }
 }
 
 sub SNMP_Set($@) {
     my ($hash, @param) = @_;
-    my $name = lc shift @param;
-    my $opt = lc shift @param;
-    my $value =  lc join(" ", @param);
+    my $name = shift @param;
+    my $opt = shift @param;
+    my $value =  join(" ", @param);
 
     if (!exists($SNMP_sets{$opt}))  {
         my @cList;
@@ -135,26 +136,28 @@ sub SNMP_Set($@) {
         return "SNMP_Set: Unknown argument $opt, choose one of " . join(" ", @cList);	
     } # error unknown opt handling
     
-	if($opt =~ /^auth$/i){
-		SNMP_savePassword($hash, $value);		
-	}
-	if($opt =~ /^power$/i){
-		if($value =~ /^(on|off)$/i){
-			SNMP_Command("$name|send|power_$1");
-			$hash->{Helper}{Running_Pid_send} = BlockingCall("SNMP_Command", $name."|send|power_$1","SNMP_CommandFinish", 30, "SNMP_CommandAborted", $name."|send") unless(exists($hash->{Helper}{Running_Pid_send}));
-		}
-	}
-	if($opt =~ /^input$/i){
-		if($value =~ /^(digital|network|storage|video|rgb).(\d+)$/i){
-			$hash->{Helper}{Running_Pid_send} = BlockingCall("SNMP_Command", $name."|send|input_$1_$2","SNMP_CommandFinish", 30, "SNMP_CommandAborted", $name."|send") unless(exists($hash->{Helper}{Running_Pid_send}));
-		}
-	}
-	if($opt =~ /^mute$/i){
-		if($value =~ /^(audio|video|both).(on|off)$/i){
-			$hash->{Helper}{Running_Pid_send} = BlockingCall("SNMP_Command", $name."|send|mute_$1_$2","SNMP_CommandFinish", 30, "SNMP_CommandAborted", $name."|send") unless(exists($hash->{Helper}{Running_Pid_send}));
-		}
-	}
+	my $jsoncoder = JSON::XS->new();
+	$jsoncoder->allow_nonref();
+	$jsoncoder->allow_blessed();
+    my %blocking_data = SNMP_DataConstructor($hash);
+    
+    if($opt =~ /^WriteSingleOID/i){
 
+        push(@{$blocking_data{getoidraw}{WriteSingleOID}},split(",",$value));
+	
+	for(my $i=0;$i < scalar @{$blocking_data{getoidraw}{WriteSingleOID}};$i++){
+		$blocking_data{getoidraw}{WriteSingleOID}[$i] =~ s/^\s+|\s+$//g;
+		my $search = $blocking_data{getoidraw}{WriteSingleOID}[$i];
+		if(my ($replace) = AttrVal($name,"WritePreDefined","") =~ /(?:^|,)\s*${search}\s*:\s*(.*?)(?:$|,)/){
+			$blocking_data{getoidraw}{WriteSingleOID}[$i] =~ s/${search}/${replace}/;
+		}
+	}
+        
+        unless(exists($hash->{Helper}{"WriteOid"})){
+            $hash->{"RUNNING"}=1;
+            $hash->{Helper}{"WriteOid"} = BlockingCall("SNMP_GetOid", $name."|".$jsoncoder->encode(\%blocking_data),"SNMP_GetOidFinish", 15, "SNMP_GetOidAborted", $name."|".$jsoncoder->encode(\%blocking_data));            
+        }
+    }
 	
 	return undef;
 }
@@ -180,32 +183,28 @@ sub SNMP_Get($@) {
 
 		return "SNMP_Get: Unknown argument $opt, choose one of " . join(" ", @cList);	
     } # error unknown opt handling
+    my $jsoncoder = JSON::XS->new();
+	$jsoncoder->allow_nonref();
+	$jsoncoder->allow_blessed();
+    my %blocking_data = SNMP_DataConstructor($hash);
 	if($opt =~ /^ReadSingleOID$/i){
-        my $jsoncoder = JSON::XS->new();
-        $jsoncoder->allow_nonref();
-        $jsoncoder->allow_blessed();
-        my %blocking_data = SNMP_DataConstructor($hash);
         
         push(@{$blocking_data{getoidraw}{ReadSingleOID}},split(",",$value)) if($value ne "");
         push(@{$blocking_data{getoidraw}{ReadSingleOID}},split(",",AttrVal($name,"ReadSingleOID",""))) if($value eq "");
         
         unless(exists($hash->{Helper}{"GetOid"})){
             $hash->{"RUNNING"}=1;
-            $hash->{Helper}{"GetOid"} = BlockingCall("SNMP_GetOid", $name."|".$jsoncoder->encode(\%blocking_data),"SNMP_GetOidFinish", 30, "SNMP_GetOidAborted", $name."|".$jsoncoder->encode(\%blocking_data));            
+            $hash->{Helper}{"GetOid"} = BlockingCall("SNMP_GetOid", $name."|".$jsoncoder->encode(\%blocking_data),"SNMP_GetOidFinish", 15, "SNMP_GetOidAborted", $name."|".$jsoncoder->encode(\%blocking_data));            
         }
     }
     if($opt =~ /^ReadListOID$/i){
-        my $jsoncoder = JSON::XS->new();
-        $jsoncoder->allow_nonref();
-        $jsoncoder->allow_blessed();
-        my %blocking_data = SNMP_DataConstructor($hash);
-        
+                
         push(@{$blocking_data{getoidraw}{ReadListOID}},split(",",$value)) if($value ne "");
         push(@{$blocking_data{getoidraw}{ReadListOID}},split(",",AttrVal($name,"ReadListOID",""))) if($value eq "");
         
         unless(exists($hash->{Helper}{"GetOid"})){
             $hash->{"RUNNING"}=1;
-            $hash->{Helper}{"GetOid"} = BlockingCall("SNMP_GetOid", $name."|".$jsoncoder->encode(\%blocking_data),"SNMP_GetOidFinish", 10, "SNMP_GetOidAborted", $name."|".$jsoncoder->encode(\%blocking_data));            
+            $hash->{Helper}{"GetOid"} = BlockingCall("SNMP_GetOid", $name."|".$jsoncoder->encode(\%blocking_data),"SNMP_GetOidFinish", 15, "SNMP_GetOidAborted", $name."|".$jsoncoder->encode(\%blocking_data));            
         }
     }
 	
@@ -225,9 +224,9 @@ sub SNMP_GetSingleOid{
         my $getoidrawstring = $getoidraw[$o];
         $getoidrawstring =~ s/^\s+|\s+$//g;
         
-        if(my ($oid) = $getoidrawstring =~ /^(.+?)(?:,|$)/){
+        if(my ($oid) = $getoidrawstring =~ /^(.+?)\s*(?:\(|$)/){
         
-            my ($instance) = $getoidrawstring =~ /;i:(\d+)\w+(?:;|$)/;
+            my ($instance) = $getoidrawstring =~ /(?:\(|\|)\s*i\s*\=\s*(\d+)/;
             $instance = "0" if(!defined($instance));
             
             
@@ -236,31 +235,32 @@ sub SNMP_GetSingleOid{
             if ($sess->{ErrorNum}) {
                 my $rc = AttrVal($name,"ReadingsCorrection","");
                 my $tag = $oid;
-                my ($userreading) = $rc =~ /(?:^|,)\s*${tag}\s*:\s*(.+?)\s*(?:,|$)/i;
+                my ($userreading) = $rc =~ /(?:^|,)\s*${tag}\s*=\s*(.+?)\s*(?:,|$)/i;
                 $userreading = $oid if(!defined($userreading) && $userreading ne $oid);
-                $blocking_data{getoid}{$userreading}{type}=1;
-                $blocking_data{getoid}{$userreading}{error}=1;
-                $blocking_data{getoid}{$userreading}{var}="Error: " . $sess->{ErrorStr};
+                $blocking_data{getoid}{$userreading."000"}{type}=1;
+                $blocking_data{getoid}{$userreading."000"}{error}=1;
+                $blocking_data{getoid}{$userreading."000"}{var}="Error: " . $sess->{ErrorStr};
             }else{
                 
                 my $rc = AttrVal($name,"ReadingsCorrection","");
                 my $tag = $vb->tag;
-                my ($userreading) = $rc =~ /(?:^|,)\s*${tag}\s*:\s*(.+?)\s*(?:,|$)/i;
+                my ($userreading) = $rc =~ /(?:^|,)\s*${tag}\s*=\s*(.+?)\s*(?:,|$)/i;
                 $userreading = $vb->tag if(!defined($userreading) && $userreading ne $vb->tag);
                 
                 my $reading = $var;
                 my $rvc = AttrVal($name,"ReadingsValueCorrection","");
                 
-                while (my ($search, $replace) = $rvc =~ /(?:^|,)\s*${userreading}\s*:\s*(.+?)\s*=\s*(.+?)\s*(?:;|,|$)/) {
+                while (my ($search, $replace) = $rvc =~ /(?:^|,)\s*${userreading}\s*\(\s*(.+?)\s*=\s*(.+?)\s*(?:\)|\||,|$)/) {
                     $reading =~ s/^${search}$/${replace}/;
                     $rvc =~ s/\s*${search}\s*=\s*${replace}\s*//;
                 }
                 
                 $reading =~ s/^\"|\"$//g;
-                $blocking_data{getoid}{$userreading . "." . $vb->iid}{error}=0;
-                $blocking_data{getoid}{$userreading . "." . $vb->iid}{iid}=$vb->iid;
-                $blocking_data{getoid}{$userreading . "." . $vb->iid}{var}=$reading if($reading ne "");
-                $blocking_data{getoid}{$userreading . "." . $vb->iid}{var}="(none)" if($reading eq "");
+		my $iidform = sprintf("%03d", $vb->iid);
+                $blocking_data{getoid}{$userreading . "." . $iidform}{error}=0;
+                $blocking_data{getoid}{$userreading . "." . $iidform}{iid}=$vb->iid;
+                $blocking_data{getoid}{$userreading . "." . $iidform}{var}=$reading if($reading ne "");
+                $blocking_data{getoid}{$userreading . "." . $iidform}{var}="(none)" if($reading eq "");
             }
         }
     }
@@ -290,30 +290,62 @@ sub SNMP_GetListOid{
             	my $userreading;
                 my $rc = AttrVal($name,"ReadingsCorrection","");
                 my $tag = $vb->tag;
-                ($userreading) = $rc =~ /(?:^|,)\s*${tag}\s*:\s*(.+?)\s*(?:,|$)/i;
+                my ($userreading) = $rc =~ /(?:^|,)\s*${tag}\s*=\s*(.+?)\s*(?:,|$)/i;
                 $userreading = $vb->tag if(!defined($userreading) && $userreading ne $vb->tag);
                 
                 my $reading = $var;
                 my $rvc = AttrVal($name,"ReadingsValueCorrection","");
                 
-                while (my ($search, $replace) = $rvc =~ /(?:^|,)\s*${userreading}\s*:\s*(.+?)\s*=\s*(.+?)\s*(?:;|,|$)/) {
+                while (my ($search, $replace) = $rvc =~ /(?:^|,)\s*${userreading}\s*\(\s*(.+?)\s*=\s*(.+?)\s*(?:\)|\||,|$)/) {
                     $reading =~ s/^${search}$/${replace}/;
                     $rvc =~ s/\s*${search}\s*=\s*${replace}\s*//;
                 }
                 $reading =~ s/^\"|\"$//g;
-                $blocking_data{getoid}{$userreading . "." . $vb->iid}{error}=0;
-                $blocking_data{getoid}{$userreading . "." . $vb->iid}{iid}=$vb->iid;
-                $blocking_data{getoid}{$userreading . "." . $vb->iid}{var}=$reading if($reading ne "");
-                $blocking_data{getoid}{$userreading . "." . $vb->iid}{var}="(none)" if($reading eq "");
+		my $iidform = sprintf("%03d", $vb->iid);
+                $blocking_data{getoid}{$userreading . "." . $iidform}{error}=0;
+                $blocking_data{getoid}{$userreading . "." . $iidform}{iid}=$vb->iid;
+                $blocking_data{getoid}{$userreading . "." . $iidform}{var}=$reading if($reading ne "");
+                $blocking_data{getoid}{$userreading . "." . $iidform}{var}="(none)" if($reading eq "");
             }
             if ($sess->{ErrorNum}) {
                 my $rc = AttrVal($name,"ReadingsCorrection","");
                 my $tag = $oid;
-                my ($userreading) = $rc =~ /(?:^|,)\s*${tag}\s*:\s*(.+?)\s*(?:,|$)/i;
+                my ($userreading) = $rc =~ /(?:^|,)\s*${tag}\s*=\s*(.+?)\s*(?:,|$)/i;
                 $userreading = $oid if(!defined($userreading) && $userreading ne $oid);
-                $blocking_data{getoid}{$userreading}{error}=1;
-                $blocking_data{getoid}{$userreading}{var}="Error: " . $sess->{ErrorStr};
+                $blocking_data{getoid}{$userreading."000"}{error}=1;
+                $blocking_data{getoid}{$userreading."000"}{var}="Error: " . $sess->{ErrorStr};
             }
+            
+        }
+    }
+    return %blocking_data;
+}
+
+sub SNMP_WriteSingleOID{
+    my (%blocking_data) = @_;
+    my $hash = $blocking_data{hash};
+    my $name = $hash->{NAME};
+
+    my $sess = new SNMP::Session(%{$blocking_data{snmpparam}});    
+    
+    my @getoidraw = (@{$blocking_data{getoidraw}{WriteSingleOID}});
+    
+    for (my $o=0;$o < scalar @getoidraw;$o++){
+        my $getoidrawstring = $getoidraw[$o];
+        $getoidrawstring =~ s/^\s+|\s+$//g;
+        
+        if(my ($oid,$oidvalue) = $getoidrawstring =~ /^(.+?)\s*=\s*(.+?)\s*(?:\(|$)/){
+	my ($instance) = $getoidrawstring =~ /(?:\(|\|)\s*i\s*\=\s*(\d+)/;
+            $instance = "0" if(!defined($instance));
+            my $vb = new SNMP::Varbind([$oid,$instance,$oidvalue ]);
+
+		# This does it!
+		$sess->set($vb);
+		if ( $sess->{ErrorNum} ) {
+		  Log3 $hash->{NAME}, 5, $hash->{NAME} . ": Got " . $sess->{ErrorStr};
+		}
+            
+            
             
         }
     }
@@ -330,7 +362,8 @@ sub SNMP_DataConstructor{
                 "ReadSingleOID"=>[],
                 "ReadSingleOIDAuto"=>[],
                 "ReadListOID"=>[],
-                "ReadListOIDAuto"=>[]
+                "ReadListOIDAuto"=>[],
+		"WriteSingleOID"=>[]
             },
             "getoid" => {},
             "snmpparam" => {
@@ -367,7 +400,8 @@ sub SNMP_GetOid($) {
 
     %blocking_data = SNMP_GetSingleOid(%blocking_data);
     %blocking_data = SNMP_GetListOid(%blocking_data);
-    
+    %blocking_data = SNMP_WriteSingleOID(%blocking_data);
+
 	return $hashname ."|".$jsoncoder->encode(\%blocking_data);
 }
 
@@ -381,17 +415,10 @@ sub SNMP_GetOidFinish($) {
     my $hash = $defs{$name};
     $hash->{"RUNNING"}=0;
     
-    my $sysname;
-    my $syslocation;
-    my $syscontact;
-    
     my $errorcount = 0;
     foreach my $k (keys %{$blocking_data{getoid}}){
         readingsSingleUpdate( $hash, "r.".$k, $blocking_data{getoid}{$k}{var}, 1 );
-        $sysname=$blocking_data{getoid}{$k}{var} if ($k =~ /^sysname.0$/i);        
-        $syslocation=$blocking_data{getoid}{$k}{var} if ($k =~ /^syslocation.0$/i);        
-        $syscontact=$blocking_data{getoid}{$k}{var} if ($k =~ /^syscontact.0$/i);
-        $errorcount++ if ($syscontact=$blocking_data{getoid}{$k}{error});
+        $errorcount++ if ($blocking_data{getoid}{$k}{error});
     }
     readingsSingleUpdate( $hash, "DataRefresh", gettimeofday(), 1 );
     readingsSingleUpdate( $hash, "DataRefreshErrors", $errorcount, 1 );
@@ -399,7 +426,7 @@ sub SNMP_GetOidFinish($) {
     
     
     
-    my $tempstate = AttrVal($name,"ReadingsToState","Name: %r.sysName.0% / Location: %r.sysLocation.0% / Contact: %r.sysContact.0%");
+    my $tempstate = AttrVal($name,"ReadingsToState","Name: %r.sysName.000% / Location: %r.sysLocation.000% / Contact: %r.sysContact.000%");
     while (my ($searchattr) = $tempstate =~ /\%(.*?)\%/) {
         my $searchattrvalue = ReadingsVal($name,$searchattr,"");
         $tempstate =~ s/\%${searchattr}\%/${searchattrvalue}/;
@@ -408,6 +435,8 @@ sub SNMP_GetOidFinish($) {
     readingsSingleUpdate( $hash, "state", $tempstate, 1 );        
     
 	delete($hash->{Helper}{"GetOid"});
+		delete($hash->{Helper}{"WriteOid"});
+
 	return;
 }
 
@@ -422,6 +451,7 @@ sub SNMP_GetOidAborted($) {
     $hash->{"RUNNING"}=0;
     
 	delete($hash->{Helper}{"GetOid"});
+	delete($hash->{Helper}{"WriteOid"});
 }
 
 sub SNMP_Attr(@) {
